e9685c88ef6f7954daddaecdee839c995a985b63
Jake Harding <jacobeharding@gmail.com>
2017-02-04 14:42:32 -0600

    Adds migrations. Setup initial tests on models.

--- repo_health/gh_projects/tests.py
+++ repo_health/gh_projects/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+"""
+tests.py - (C) Copyright - 2017
+This software is copyrighted to contributors listed in CONTRIBUTIONS.md.
 
-# Create your tests here.
+SPDX-License-Identifier: MIT
+
+Author(s) of this file:
+  J. Harding
+
+Test the generated models are functioning as expected.
+"""
+
+
+from django.test import TestCase
+from factory.django import DjangoModelFactory
+
+from .models import GhProject
+
+class ProjectFactory(DjangoModelFactory):
+    class Meta:
+        model = GhProject
+
+class TestGhProject(TestCase):
+    
+    # def setUp(self):
+    #     """Setup ran before every test."""
+    #     pass
+
+    def test_get_project(self):
+        project = GhProject.objects.get(id=1)
+        self.assertEqual(None, project)        
