8ceb4f8061bdb78312df4b5ca3c19318bcdd7d6e
Benjamin Parish <benjaminparish628@gmail.com>
2017-02-13 18:32:15 -0600

    Added license identifiers to ui files

--- repo_health/index/static/src/app.js
+++ repo_health/index/static/src/app.js
@@ -1,3 +1,16 @@
+/*
+* app.js - (C) Copyright - 2017
+* This software is copyrighted to contributors listed in CONTRIBUTIONS.md.
+*
+* SPDX-License-Identifier: MIT
+*
+* Author(s) of this file:
+* @bparish628
+*
+* This is where the angular app gets bootstraped.
+* It will set up all the global configuration.
+*/
+
 (function () {
   angular.module('repo-health', [
     'components'
