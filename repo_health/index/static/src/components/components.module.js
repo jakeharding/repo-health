9d44f862266d5c7d3a1dee586db985fbb168dc16
Benjamin Parish <benjaminparish628@gmail.com>
2017-02-13 21:26:28 -0600

    Added to components.module.js

--- repo_health/index/static/src/components/components.module.js
+++ repo_health/index/static/src/components/components.module.js
@@ -1,3 +1,15 @@
+/*
+* component.module.js - (C) Copyright - 2017
+* This software is copyrighted to contributors listed in CONTRIBUTIONS.md.
+*
+* SPDX-License-Identifier: MIT
+*
+* Author(s) of this file:
+* @bparish628
+*
+* This is the definitions for all of the components used in this application.
+*/
+
 (function () {
   angular.module('components', [
     'components.search'
