3e767ba58988d53f0fe1927b4124e87762d2f555
Jake Harding <jacobeharding@gmail.com>
2017-03-12 21:39:48 -0500

    Add mixin to get list of count per month for past year of a queryset.

---
+++ repo_health/index/mixins/__init__.py
@@ -0,0 +1,13 @@
+"""
+__init__.py - (C) Copyright - 2017
+This software is copyrighted to contributors listed in CONTRIBUTIONS.md.
+
+SPDX-License-Identifier: MIT
+
+Author(s) of this file:
+  J. Harding
+
+Mixins to be used across packages.  Import them here.
+"""
+
+from .CountForPastYearMixin import CountForPastYearMixin
