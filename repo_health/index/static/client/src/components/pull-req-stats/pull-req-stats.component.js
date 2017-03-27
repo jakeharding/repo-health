6c35021b6b3ade1acb2d6e29b2a744aab5cbf021
Jake Harding <jacobeharding@gmail.com>
2017-03-27 14:41:21 -0500

    Remove example code for chart js.

--- repo_health/index/static/client/src/components/pull-req-stats/pull-req-stats.component.js
+++ repo_health/index/static/client/src/components/pull-req-stats/pull-req-stats.component.js
@@ -21,43 +21,6 @@ const pullReqStatsComponent = {
     loadingStats = true;
     loadingMsg = "Loading pull request stats...";
     numOfStatsSections = 0;
-
-    labels = ['Jan', 'feb', 'Mar', 'April', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    series = ['Series A', 'Series B'];
-    data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
-    onClick = function (points, evt) {
-      console.log(points, evt);
-    };
-    onHover = function (points) {
-      if (points.length > 0) {
-        console.log('Point', points[0].value);
-      } else {
-        console.log('No point');
-      }
-    };
-    datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-
-    options = {
-      scales: {
-        yAxes: [
-          {
-            id: 'y-axis-1',
-            type: 'linear',
-            display: true,
-            position: 'left'
-          },
-          {
-            id: 'y-axis-2',
-            type: 'linear',
-            display: true,
-            position: 'right'
-          }
-        ]
-      }
-    };
     
     constructor( $http, $filter ) {
       super($http, $filter);
