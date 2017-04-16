36092f8647e1f1d8ad325b2b1535a4699648168d
Jake Harding <jacobeharding@gmail.com>
2017-04-15 21:41:52 -0500

    Change test_settings to travis_settings. Add .travis.yml.

--- repo_health/travis_settings.py
+++ repo_health/travis_settings.py
@@ -3,11 +3,13 @@ from .settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'msrtest',
+        'NAME': 'repo_health',
         'USER': 'root',
         'PASSWORD': '',
     }
 }
+
+# Settings for sqlite3 tests will use an in memory database and decrease test time.
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
